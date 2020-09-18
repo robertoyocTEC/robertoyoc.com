ceb6765d53dc7dca5d13d05afc76b1f7111d420c
Roberto Yoc <A01172599@itesm.mx>
2020-09-18 17:56:09 -0500

    Watson assistant integrated

---
+++ api.py
@@ -0,0 +1,140 @@
+# API
+import os
+import json
+import logging
+import requests
+
+import flask
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_cors import CORS
+from dotenv import load_dotenv
+from flask_api import status
+
+from jsonschema import validate, ValidationError
+from ibm_watson import AssistantV2, ApiException
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from flask import jsonify
+
+load_dotenv()
+
+app = Flask(__name__)
+api = Api(app)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+assistant_api_key = os.getenv("assistant_api_key")
+
+request_data = {
+            "assistant_api_key": os.getenv("assistant_api_key"),
+            "assistant_url": os.getenv("assistant_url"),
+            "assistant_version": os.getenv("assistant_version"),
+            "assistant_id": os.getenv("assistant_id")
+        } 
+
+def watson_create_session():
+    iam_apikey = request_data.get("assistant_api_key")
+    assistant_url = request_data.get("assistant_url")
+    assistant_version = request_data.get('assistant_version')
+
+    assistant = watson_instance(iam_apikey, assistant_url, assistant_version)
+
+    try:
+        watson_session = assistant.create_session(
+            assistant_id=request_data.get("assistant_id")
+        ).get_result()
+        watson_session_id = watson_session["session_id"]
+    except KeyError:
+        _logger.error("The session wasn't created")
+        return jsonify({"error": "Error creating the session"}), status.HTTP_503_SERVICE_UNAVAILABLE
+
+    return watson_session_id
+
+
+def watson_response(session_id, message):
+    iam_apikey = request_data.get("assistant_api_key")
+    assistant_url = request_data.get("assistant_url")
+    assistant_version = request_data.get('assistant_version')
+
+    assistant = watson_instance(iam_apikey, assistant_url, assistant_version)
+    context = request_data.get('context') if 'context' in request_data else {}
+    watson_session_id = session_id
+
+    try:
+        watson_response = assistant.message(
+            assistant_id=request_data.get('assistant_id'),
+            session_id=watson_session_id,
+            input={
+                'message_type': 'text',
+                'text': message,
+                'options': {
+                    'return_context': True
+                }
+            },
+            context=context
+        ).get_result()
+    except ValueError as ex:
+        _logger.error("Value error: %s", ex)
+        return jsonify({'error': "Value error"}), status.HTTP_500_INTERNAL_SERVER_ERROR
+    except ApiException:
+        try:
+            watson_session = assistant.create_session(
+                assistant_id=request_data.get("assistant_id")
+            ).get_result()
+            watson_session_id = watson_session["session_id"]
+
+            watson_response = assistant.message(
+                assistant_id=request_data.get('assistant_id'),
+                session_id=watson_session_id,
+                input={
+                    'message_type': 'text',
+                    'text': message,
+                    'options': {
+                        'return_context': True
+                    }
+                },
+                context=context
+            ).get_result()
+        except KeyError:
+            _logger.error("The session wasn't created")
+            return jsonify({"error": "Error creating the session"}), status.HTTP_503_SERVICE_UNAVAILABLE
+
+    try:
+        del watson_response["context"]["global"]["session_id"]
+    except:
+        pass
+
+    response = {
+        "response": watson_response,
+        "session_id": watson_session_id
+    }
+
+    return {
+        'text': response.get('response').get('output').get('generic')[0].get('text'),
+        'intent': response.get('response').get('output').get('intents')[0].get('intent')
+    }
+
+def watson_instance(iam_apikey: str, url: str, version: str = "2019-02-28") -> AssistantV2:
+    try:
+        authenticator = IAMAuthenticator(iam_apikey)
+        assistant = AssistantV2(
+            authenticator=authenticator,
+            version=version
+        )
+        assistant.set_service_url(url)
+    except ApiException as error:
+        _logger.error("%s - %s", error.code, error.message)
+        return jsonify({'error': str(error.message)}), error.code
+
+    return assistant
+
+
+class GET_MESSAGE(Resource):
+    def post(self):
+        response = watson_response(watson_create_session(), request.json["message"])
+        return response
+
+
+api.add_resource(GET_MESSAGE, '/getMessage')  # Route_1
+
+if __name__ == '__main__':
+    app.run(port='5002')
--- src/App.js
+++ src/App.js
@@ -1,26 +1,162 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component, useState, useEffect} from 'react';
+import parse from 'html-react-parser';
 import './App.css';
+const axios = require('axios');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function Chat() {
+//   const [message, setMessage] = useState('');
+//   const [messages, addMessage] = useState([]); 
+
+//   useEffect(() => {
+//     axios.post('http://127.0.0.1:5002/getMessage', {
+//       message: "hi"
+//     })
+//     .then (res => {
+//       console.log(res.data.text);
+//       messages.push({
+//         message: res.data.text,
+//         from: 'chatbot'
+//       })
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     });
+//   }, []);
+
+//   function postMessage() {
+//     if(message === '') {
+//       alert('Message should not be empty');
+//     } else {
+//       messages.push(
+//         {
+//           message: message,
+//           from: 'user'
+//         }
+//       );
+//       setMessage('');
+//       console.log('fetching...');
+//       axios.post('http://127.0.0.1:5002/getMessage', {
+//       message: message
+//       })
+//       .then (res => {
+//         console.log(res.data.text);
+//         messages.push({
+//           message: res.data.text,
+//           from: 'chatbot'
+//         });
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+//     }
+//   }
+
+//   function showData() {
+//     console.log(messages);
+//   }
+
+//   return (
+//     <div className="chat">
+//       <div className="main">
+//         <div className="messages">
+//           {messages.map((message, index) => {
+//             return (
+              // <div key={index} className={message.from === 'user' ? 'from-user': 'from-chatbox'}>
+              //   {message.from === 'user' ? 
+              //     <p className={message.from === 'user' ? 'user-msg': 'chatbot-msg'}>{message.message}</p> :
+              //     parse(message.message)
+              //   }
+//               </div>
+//             );
+//           })}
+//         </div>
+//         <div className="send">
+//           <input type="text" value={message} placeholder="Type your message here" onChange={(e) => setMessage(e.target.value)} />
+//           <button onClick={postMessage}><i className="material-icons">send</i></button>
+//         </div>
+//       </div>
+//       <button style={{position: "fixed", top: "0", left: "0"}} onClick={showData}>Get Data</button>
+//     </div>
+//   );
+// }
+
+class Chatbox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+      message: ''
+    }
+    this.addMessage = this.addMessage.bind(this);
+    this.postMessage = this.postMessage.bind(this);
+  }
+
+  addMessage(event) {
+    this.setState({
+      message: event.target.value
+    });
+  }
+
+  postMessage() {
+    if(this.state.message === '') {
+      alert('Message should not be empty');
+    } else {
+      this.setState({
+        messages: [
+          ...this.state.messages,
+          {
+            message: this.state.message,
+            from: 'user'
+          }
+        ],
+        message: ''
+      });
+      console.log('fetching...');
+      axios.post('http://127.0.0.1:5002/getMessage', {
+      message: this.state.message
+      })
+      .then (res => {
+        console.log(res.data.text);
+        this.setState({
+          messages: [
+            ...this.state.messages, 
+            {
+              message: res.data.text,
+              from: 'chatbox'
+            }
+          ]
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="chat">
+        <div className="main">
+          <div className="messages">
+            {this.state.messages.map((message, index) => {
+              return (
+                message.from === 'user' ? 
+                <div key={index} className="from-user">
+                    <p className={message.from === 'user' ? 'user-msg': 'chatbot-msg'}>{message.message}</p> 
+                </div>:
+                <div key={index} className="from-chatbox" dangerouslySetInnerHTML={{ __html: message.message }}></div>
+              );
+            })}
+          </div>
+          <div className="send">
+            <input type="text" value={this.state.value} placeholder="Type your message here" onChange={this.addMessage} />
+            <button onClick={this.postMessage}><i className="material-icons">send</i></button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
 }
 
-export default App;
+export default Chatbox;
